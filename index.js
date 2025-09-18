ec754a6ef24681b38f3f0e7834543b9abc6ff986
mahmudulhasan71 <mdmahmudulhasansheikh01@gmail.com>
2025-09-18 22:00:57 +0600

    added copy button functionality

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ for(const heartButton of heartButtons){
 
     })
 }
+
+//copy button functionality
+const copyButtons = document.querySelectorAll('.copy-btn');
+const copyCount = document.getElementById('copy-count');
+let copy = 0;
+
+for (const copyButton of copyButtons){
+    copyButton.addEventListener('click',function(){
+        const card = copyButton.closest('.card')
+        const serviceName = card.querySelector('h1').innerText
+        const serviceNumber = card.querySelector('h3').innerText
+        navigator.clipboard.writeText(serviceNumber)
+        alert(`Number copied.. :${serviceNumber}`)
+        copy ++
+        copyCount.innerText=copy
+    })
+}
+
